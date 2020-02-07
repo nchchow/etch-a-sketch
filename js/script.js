383915ec8757fc5435a7c62f889f2264ca2d41d7
nchchow <nelsonchow91.nc@gmail.com>
2020-02-07 13:21:19 -0800

    Placed 16x16 squares in a single column

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 const container = document.querySelector("#container");
 
 // create 16x16 square divs
-const square = document.createElement("div");
-square.classList.add("square");
-
-container.appendChild(square);
+for(var i = 0; i < 16*16; ++i) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}
