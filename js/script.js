3f7ea8a020c0867d072308e76feec4f86f311f06
nchchow <nelsonchow91.nc@gmail.com>
2020-02-07 13:01:05 -0800

    Placed one square in

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // html references
 const container = document.querySelector("#container");
+
+// create 16x16 square divs
+const square = document.createElement("div");
+square.classList.add("square");
+
+container.appendChild(square);
