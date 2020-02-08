192c13f41cd861444655ab846903e70420a42e83
nchchow <nelsonchow91.nc@gmail.com>
2020-02-07 17:56:55 -0800

    Add prompt to create a new grid based on input

--- js/script.js
+++ js/script.js
@@ -4,22 +4,41 @@ const resetbtn = document.querySelector("#reset");
 
 createGrid(16); // initial grid layout
 
-// create 16x16 square divs
-function createGrid(gridsPerSide) {
-    for (var i = 0; i < gridsPerSide * gridsPerSide; ++i) {
+// create n x n square divs
+function createGrid(n) {
+    for (var i = 0; i < n * n; ++i) {
         const square = document.createElement("div");
         square.classList.add("square");
+
+        // modify square size
+        let squareWidth = 460 / n;
+        square.style.width = squareWidth + "px";
+        square.style.height = squareWidth + "px";
+
         square.addEventListener("mouseover", (e) => {
             square.style.backgroundColor = "black";
         });
+        container.style.gridTemplateColumns = "repeat(" + n + ", " + square.style.width + ")";
         container.appendChild(square);
     }
 }
 
-// reset button event listener
+// reset button resets all squares to white
 resetbtn.addEventListener("click", (e) => {
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => {
-        square.style.backgroundColor = "white";
+        container.removeChild(square);
     });
+    let n = promptInput();
+    createGrid(n);
 });
+
+// prompts for input, returns 16 if invalid entry
+function promptInput() {
+    var number = prompt("Enter a number to determine the size of the next grid");
+    if(parseInt(number)) {
+        return number;
+    } else {
+        return 16;
+    }
+}
