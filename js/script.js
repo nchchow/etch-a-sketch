099a9bcd73e41ac1c161d786eee161051be491d6
nchchow <nelsonchow91.nc@gmail.com>
2020-02-07 16:39:59 -0800

    Add reset button that resets the grids back to white

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // html references
 const container = document.querySelector("#container");
+const resetbtn = document.querySelector("#reset");
 
 // create 16x16 square divs
 for(var i = 0; i < 16*16; ++i) {
@@ -7,7 +8,13 @@ for(var i = 0; i < 16*16; ++i) {
     square.classList.add("square");
     square.addEventListener("mouseover", (e) => {
         square.style.backgroundColor = "black";
-    })
+    });
     container.appendChild(square);
 }
 
+resetbtn.addEventListener("click", (e) => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+});
