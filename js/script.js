3e36741b4680ef07d89c82dedc50a6b9d309845f
nchchow <nelsonchow91.nc@gmail.com>
2020-02-07 17:30:28 -0800

    Refactor grid creation logic into createGrid function

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@
 const container = document.querySelector("#container");
 const resetbtn = document.querySelector("#reset");
 
+createGrid(16); // initial grid layout
+
 // create 16x16 square divs
-for(var i = 0; i < 16*16; ++i) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", (e) => {
-        square.style.backgroundColor = "black";
-    });
-    container.appendChild(square);
+function createGrid(gridsPerSide) {
+    for (var i = 0; i < gridsPerSide * gridsPerSide; ++i) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("mouseover", (e) => {
+            square.style.backgroundColor = "black";
+        });
+        container.appendChild(square);
+    }
 }
 
+// reset button event listener
 resetbtn.addEventListener("click", (e) => {
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => {
